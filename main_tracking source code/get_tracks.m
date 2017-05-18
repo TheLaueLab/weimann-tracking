0520fa634b7b71daf8da7fdda99aa3c9a5edcd36
Kai Wohlfahrt <kjw53@cam.ac.uk>
2017-05-18 14:06:43 +0100

    Indentation fixes for get_tracks
    Need to edit and it was really confusing

--- main_tracking source code/get_tracks.m
+++ main_tracking source code/get_tracks.m
@@ -8,31 +8,28 @@
 
 
 function [results] = get_tracks(parameters,setup)
+  %%define input for track function
+  param.mem=parameters.memory;
+  param.dim=2;
+  param.good=parameters.minLength;
+  param.quiet=1;
 
-%define input for track function
-param.mem=parameters.memory;
-param.dim=2;
-param.good=parameters.minLength;
-param.quiet=1;
+  stack_directory_cell = setup.directory;
+  Mean_Tracklength_allcells=[];
+  Tracklength_allcells=[];
+  mean_SNR_raw_track_allcells = [];
+  trackdensity_per_cell_allcells = [];
 
-stack_directory_cell = setup.directory;
-Mean_Tracklength_allcells=[];
-Tracklength_allcells=[];
-mean_SNR_raw_track_allcells = [];
-trackdensity_per_cell_allcells = [];
+  number_stack = length(stack_directory_cell);
 
-number_stack = length(stack_directory_cell);
-
-%loop over all stacks
-for stack_count=1:number_stack
-
-    %clear all var1iables except of those specified after except
+  %%loop over all stacks
+  for stack_count=1:number_stack
+    %%clear all var1iables except of those specified after except
     clearvars -except setup folders number_stack stack_directory* trackdensity_per_cell_allcells Mean_Tracklength_allcells Tracklength_allcells blink_freq_Hertz_all intensity_track_allcells mean_pp_distance_allcells param exp_time parameters stack_count save_dir_all exp_name withvideo_simulation fraction_fully_reconstructed_tracks_allcells mean_SNR_raw_track_allcells
 
     clear Tracks info4Tracks Results* newY_cell filtered_image* length_tracks I result* im
-
     folders = stack_directory_cell{stack_count};
-    %h1=waitbar(0, counter);
+    %%h1=waitbar(0, counter);
     mean_SNR_raw_track_all = [];
 
     fName = folders;
@@ -55,94 +52,87 @@ for stack_count=1:number_stack
     file2 = strcat(folders, '/', 'files4tracking.mat');
     load(file2);
 
-    %Now we gonna create a matrix in a form which is suited for the function track
-    %based on crockers code
+    %%Now we gonna create a matrix in a form which is suited for the function track
+    %%based on crockers code
     Results_final=[];
 
     for t=1:T
-        clear Temp;
-        if isempty(xyIfilIrawSNRfilSNRraw{t})==1
-            xyIfilIrawSNRfilSNRraw{t} = [ 0 0 0 0 0 0];
+      clear Temp;
+      if isempty(xyIfilIrawSNRfilSNRraw{t})==1
+        xyIfilIrawSNRfilSNRraw{t} = [ 0 0 0 0 0 0];
+      end
+      Temp = xyIfilIrawSNRfilSNRraw{t};
+      Temp = Temp(:,1:2);
+      if ~isempty(Temp)
+        if Temp(1) ~=0
+          Temp(:,3)=t;
+          Temp=Temp';
+          Results_final=[Results_final Temp];
         end
-        Temp = xyIfilIrawSNRfilSNRraw{t};
-        Temp = Temp(:,1:2);
-        if ~isempty(Temp)
-            if Temp(1) ~=0
-               Temp(:,3)=t;
-               Temp=Temp';
-               Results_final=[Results_final Temp];
-            end
-        end
+      end
     end
 
     Results_final=Results_final';
 
-    %applying of track funtion
+    %%applying of track funtion
     if ~isempty(Results_final)
-    result_tracks=track(Results_final,parameters.max_step,param);
+      result_tracks=track(Results_final,parameters.max_step,param);
 
-    length_tracks = [];
-     if ~isempty(result_tracks)
-
-     %add I raw, SNR fil, SNR raw to result_tracks
-     i_result_tracks = length(find(result_tracks(result_tracks(:,1)~=0)));
-     result_tracks = result_tracks(1:i_result_tracks,:);
-      for i=1:i_result_tracks
-            a=result_tracks(i,:);
-            tt=xyIfilIrawSNRfilSNRraw{a(3)};
-            ll=find(tt==a(1));
-            temp=tt(ll,:);
-            result_tracks_SNR_temp=[a,temp(4:6)];
-            result_tracks_SNR(i,:)=result_tracks_SNR_temp;
-      end
-
-     ntrack = 1;
-     figure();
-
-      for nt=1:result_tracks((size(result_tracks,1)-1),4)
-      clear temp a
-      a=find((result_tracks(:,4)==nt));
-
-        if length(a) > param.good
-           temp=result_tracks(a,3);
-           %save frame number, x,y position, I raw, SNR fil, SNR raw
-           temp(:,2:6)=result_tracks_SNR(a,[1,2,5,6,7]);
-
-                %%%Calculate mean intensity of track
-                SNR_raw_track = temp(:,6);
-                mean_SNR_raw_track = mean(SNR_raw_track);
-
-                order4track = num2str(ntrack, '%3d');
-                filename = strcat(save_dir,'/track_', order4track, '.dat');
-                dlmwrite(filename,temp,'newline','pc');
-                length_tracks(ntrack)=length(a);
-                %%%Plot ensemble of tracks
-                drawTest_all(temp, setup, ntrack,parameters, stack_count)
-                mean_SNR_raw_track_all = [mean_SNR_raw_track_all mean_SNR_raw_track'];
-                ntrack = ntrack +1;
-            %end
+      length_tracks = [];
+      if ~isempty(result_tracks)
+        %%add I raw, SNR fil, SNR raw to result_tracks
+        i_result_tracks = length(find(result_tracks(result_tracks(:,1)~=0)));
+        result_tracks = result_tracks(1:i_result_tracks,:);
+        for i=1:i_result_tracks
+          a=result_tracks(i,:);
+          tt=xyIfilIrawSNRfilSNRraw{a(3)};
+          ll=find(tt==a(1));
+          temp=tt(ll,:);
+          result_tracks_SNR_temp=[a,temp(4:6)];
+          result_tracks_SNR(i,:)=result_tracks_SNR_temp;
         end
 
-      end
+        ntrack = 1;
+        figure();
 
+        for nt=1:result_tracks((size(result_tracks,1)-1),4)
+          clear temp a
+          a=find((result_tracks(:,4)==nt));
+
+          if length(a) > param.good
+            temp=result_tracks(a,3);
+            %%save frame number, x,y position, I raw, SNR fil, SNR raw
+            temp(:,2:6)=result_tracks_SNR(a,[1,2,5,6,7]);
+
+            %%Calculate mean intensity of track
+            SNR_raw_track = temp(:,6);
+            mean_SNR_raw_track = mean(SNR_raw_track);
+
+            order4track = num2str(ntrack, '%3d');
+            filename = strcat(save_dir,'/track_', order4track, '.dat');
+            dlmwrite(filename,temp,'newline','pc');
+            length_tracks(ntrack)=length(a);
+            %%Plot ensemble of tracks
+            drawTest_all(temp, setup, ntrack,parameters, stack_count)
+            mean_SNR_raw_track_all = [mean_SNR_raw_track_all mean_SNR_raw_track'];
+            ntrack = ntrack +1;
+          end
+        end
 
         set(gca,'YDir','reverse')
         title(['Trajectory Ensemble']);
         fName_hist_all = strcat(save_dir,'/','Trajectory Ensemble.fig');
         saveas(gcf,fName_hist_all)
 
-
-
-     else
-
-         errordlg('Choose a different end point for parameters.endt','Track error');
-         clear all;
-         return;
-            length_tracks = 0;
-     end
-
       else
-            length_tracks = 0;
+        errordlg('Choose a different end point for parameters.endt','Track error');
+        clear all;
+        return;
+        length_tracks = 0;
+      end
+
+    else
+      length_tracks = 0;
     end
 
 
@@ -155,14 +145,14 @@ for stack_count=1:number_stack
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-    %calculate mean spot density
+    %%calculate mean spot density
     number_tracks = sum(length_tracks);
     Pixelsize = parameters.PixelSize/1000; %[um]
     Image_area = setup.M(stack_count)*setup.N(stack_count)*(Pixelsize)^2;
     density_per_cell = number_tracks/T;
     density_perframe = density_per_cell/Image_area;
 
-    %plot histogram of track length for each cell
+    %%plot histogram of track length for each cell
     figure(),hist(length_tracks);
     title(['#Tracks = ',num2str(length(length_tracks)), ', mean length = ',num2str(mean(length_tracks)), ', track density = ' num2str(round( density_perframe*1000)/1000),' per um^2', ' / ', num2str(round( density_per_cell*1000)/1000),' tracks per frame']);
     fName_hist_all = strcat(save_dir,'/','Histogram_tracklength.fig');
@@ -173,20 +163,20 @@ for stack_count=1:number_stack
     savefile = strcat(parameters.exp_name,'/','Tracklength.mat');
     save(savefile,'length_tracks');
 
-    %plot histogram of mean track intensities
+    %%plot histogram of mean track intensities
     if isempty(mean_SNR_raw_track_all) ~= 1
-    figure(),hist(mean_SNR_raw_track_all,sqrt(length(mean_SNR_raw_track_all )))
-    title(['cell = ',num2str(stack_count), ', # tracks = ',num2str( length(mean_SNR_raw_track_all ))]);
-    fName_hist_all = strcat(save_dir,'/','Histogram_SNR_raw.fig');
-    ylabel('Frequency','fontsize',12,'fontweight','b')
-    xlabel('Mean SNR raw per track','fontsize',12,'fontweight','b')
-    saveas(gcf,fName_hist_all)
+      figure(),hist(mean_SNR_raw_track_all,sqrt(length(mean_SNR_raw_track_all )))
+      title(['cell = ',num2str(stack_count), ', # tracks = ',num2str( length(mean_SNR_raw_track_all ))]);
+      fName_hist_all = strcat(save_dir,'/','Histogram_SNR_raw.fig');
+      ylabel('Frequency','fontsize',12,'fontweight','b')
+      xlabel('Mean SNR raw per track','fontsize',12,'fontweight','b')
+      saveas(gcf,fName_hist_all)
     end
 
-    %make videos
+    %%make videos
     if parameters.withvideo == 1
-        figure();
-        makeavi64bit_real_data(parameters,I,T,save_dir)
+      figure();
+      makeavi64bit_real_data(parameters,I,T,save_dir)
     end
 
     mean_SNR_raw_track_allcells = [mean_SNR_raw_track_allcells mean(mean_SNR_raw_track_all)];
@@ -195,49 +185,45 @@ for stack_count=1:number_stack
     trackdensity_per_cell_allcells = [trackdensity_per_cell_allcells density_perframe ];
 
     close all
+  end
 
+  results.meanSNRrawpertrack = mean_SNR_raw_track_allcells;
+  results.meantracklength = Mean_Tracklength_allcells;
 
+  save_dir = strcat(parameters.exp_name,'/','spot detection Results');
 
-end
-
-results.meanSNRrawpertrack = mean_SNR_raw_track_allcells;
-results.meantracklength = Mean_Tracklength_allcells;
-
-    save_dir = strcat(parameters.exp_name,'/','spot detection Results');
-
-    figure(),plot(1:1:number_stack,trackdensity_per_cell_allcells,'--rs','LineWidth',2,...
+  figure(),plot(1:1:number_stack,trackdensity_per_cell_allcells,'--rs','LineWidth',2,...
                 'MarkerEdgeColor','k',...
                 'MarkerFaceColor','g',...
                 'MarkerSize',10)
-    title(['All cells, Track density per cell over cell per um^2']);
-    fName_hist_all = strcat(save_dir,'/','Trackdensity.fig');
-    ylim([0 max(trackdensity_per_cell_allcells)])
-    ylabel('Trackdensity per cell','fontsize',12,'fontweight','b')
-    xlabel('Cell index','fontsize',12,'fontweight','b')
-    saveas(gcf,fName_hist_all)
+  title(['All cells, Track density per cell over cell per um^2']);
+  fName_hist_all = strcat(save_dir,'/','Trackdensity.fig');
+  ylim([0 max(trackdensity_per_cell_allcells)])
+  ylabel('Trackdensity per cell','fontsize',12,'fontweight','b')
+  xlabel('Cell index','fontsize',12,'fontweight','b')
+  saveas(gcf,fName_hist_all)
 
-    figure(),plot(1:1:number_stack,Mean_Tracklength_allcells,'--rs','LineWidth',2,...
+  figure(),plot(1:1:number_stack,Mean_Tracklength_allcells,'--rs','LineWidth',2,...
                 'MarkerEdgeColor','k',...
                 'MarkerFaceColor','g',...
                 'MarkerSize',10)
-    title(['All cells, Mean track length over cell']);
-    fName_hist_all = strcat(save_dir,'/','Tracklength.fig');
-    ylim([0 max(Mean_Tracklength_allcells)])
-    ylabel('Mean track length','fontsize',12,'fontweight','b')
-    xlabel('Cell index','fontsize',12,'fontweight','b')
-    saveas(gcf,fName_hist_all)
+  title(['All cells, Mean track length over cell']);
+  fName_hist_all = strcat(save_dir,'/','Tracklength.fig');
+  ylim([0 max(Mean_Tracklength_allcells)])
+  ylabel('Mean track length','fontsize',12,'fontweight','b')
+  xlabel('Cell index','fontsize',12,'fontweight','b')
+  saveas(gcf,fName_hist_all)
 
-    figure(),plot(1:1:number_stack,mean_SNR_raw_track_allcells,'--rs','LineWidth',2,...
+  figure(),plot(1:1:number_stack,mean_SNR_raw_track_allcells,'--rs','LineWidth',2,...
                 'MarkerEdgeColor','k',...
                 'MarkerFaceColor','g',...
                 'MarkerSize',10)
-    title(['All cells, mean SNR per track over cell']);
-    fName_hist_all = strcat(save_dir,'/','SNR_pertrack.fig');
-    ylim([0 max(Mean_Tracklength_allcells)])
-    ylabel('mean SNR raw','fontsize',12,'fontweight','b')
-    xlabel('Cell index','fontsize',12,'fontweight','b')
-    saveas(gcf,fName_hist_all)
+  title(['All cells, mean SNR per track over cell']);
+  fName_hist_all = strcat(save_dir,'/','SNR_pertrack.fig');
+  ylim([0 max(Mean_Tracklength_allcells)])
+  ylabel('mean SNR raw','fontsize',12,'fontweight','b')
+  xlabel('Cell index','fontsize',12,'fontweight','b')
+  saveas(gcf,fName_hist_all)
 
-
-    savefile = strcat(parameters.exp_name,'/','Results.mat');
-    save(savefile, 'setup','parameters','results');
+  savefile = strcat(parameters.exp_name,'/','Results.mat');
+  save(savefile, 'setup','parameters','results');
